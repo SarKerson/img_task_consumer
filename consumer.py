17783b35f03bc2a191c2b21f71091fc7ee9cb01e
yaoleiqi <yaoleiqi@bytedance.com>
2020-05-23 12:11:39 +0800

    perf: add logs

--- consumer.py
+++ consumer.py
@@ -1,6 +1,8 @@
 import json
 import io
 import redis
+import logging
+
 
 from kafka import KafkaConsumer
 from google.cloud import storage
@@ -22,6 +24,17 @@ consumer = KafkaConsumer(
 # cloud storage
 storage_client = storage.Client()
 bucket = storage_client.bucket(BUCKET_NAME)
+
+
+import os
+ROOT = os.path.dirname(os.path.abspath(__file__))
+logfile = "{0}/logs/app.log.{1}".format(ROOT, time.strftime("%Y%m%d"))
+fh = logging.FileHandler(filename=logfile)
+fmt = logging.Formatter(fmt="%(asctime)s|%(levelname)s|%(process)d|%(message)s", datefmt="%Y/%m/%d %H:%M:%S")
+fh.setFormatter(fmt)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(fh)
 
 
 class TaskMeta(object):
@@ -105,7 +118,9 @@ SUC = 0
 def process(task_id):
     # get meta info
     meta = TaskMeta.from_redis(task_id)
+    logging.info('[process] msg: %s', task_id)
     if not meta:
+        logging.error('[process] get from_redis failed, task_id: %s', task_id)
         return ERR_READ_REDIS_FAILED
 
     # inference
@@ -113,15 +128,20 @@ def process(task_id):
 
     # upload result
     obj_name = upload_img(meta.task_id, result_img)
+    logging.info('[process] upload_img: %s, obj_name: %s', task_id, obj_name)
     if not obj_name:
+        logging.error('[process] upload_img failed, task_id: %s', task_id)
         return ERR_WRITE_CLOUD_STORAGE_FAILED
     output_url = URL % obj_name
 
     # update meta info
     meta.output_url = output_url
     meta.proc_status = TaskMeta.TASK_STATUS_SUC
+    logging.info('[process] meta to update: %s', meta.to_dict())
     if not meta.update():
+        logging.error('[process] meta.update failed, task_id: %s', task_id)
         return ERR_WRITE_REDIS_FAILED
+    logging.info('[process] done task: %s', task_id)
     return SUC
 
 
@@ -129,7 +149,9 @@ def run():
     for msg in consumer:
         if msg.key != 'commit':
             continue
-        process(msg.value)
+        logging.info('[run] msg: %s', msg.value)
+        r = process(msg.value)
+        logging.info('[run] done msg: %s', r)
 
 
 def test():
